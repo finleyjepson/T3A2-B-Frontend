0580f8e942526358cfcc649f6d77629217d04fac
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-19 14:00:11 +1100

    build: initialised Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
-}
+    return (
+        <>
+        <h1 className="text-3xl font-bold underline">Hello</h1>
+        <div className="bg-blue-500">Test</div>
+        </>
+    )
+    }
 
 export default App
