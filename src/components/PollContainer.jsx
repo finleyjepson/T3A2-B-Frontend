0f0be64c3d2fde245758c514388e8d21b7f67526
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-20 15:17:59 +1100

    feat: embedded poll

--- src/components/PollContainer.jsx
+++ src/components/PollContainer.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 function PollContainer() {
     return (
-        <h1>test</h1>
+        <>
+            {/* Embedding poll made with https://strawpoll.com/ */}
+            <div className="flex p-4 justify-center">
+                <div className="strawpoll-embed" style={{ height: '644px', maxWidth: '640px', width: '100%', margin: '0 auto', display: 'flex', flexDirection: 'column' }}>
+                    <iframe
+                        title="StrawPoll Embed"
+                        src="https://strawpoll.com/embed/GJn47PbWbyz"
+                        style={{ position: 'static', visibility: 'visible', display: 'block', width: '100%', flexGrow: 1 }}
+                        frameBorder="0"
+                        allowFullScreen
+                        allowTransparency
+                    >
+                        Loading...        
+                    </iframe>
+                    <script async src="https://cdn.strawpoll.com/dist/widgets.js" charSet="utf-8"></script>
+                </div>
+            </div>    
+        </>
     );
 }
 
