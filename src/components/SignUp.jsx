ecd9efdcb014ffe522d3dbdc97edfdd4fd08c261
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-21 17:53:26 +1100

    feat: only show password match error after submission

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -10,7 +10,7 @@ export default function Signup() {
     })
 
     // State variable for whether form is submitted (for password check)
-    const setFormSubmitted = useState(false);
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     // Synchronise the form data state with user input changes 
     function handleChange(e) { 
@@ -28,7 +28,6 @@ export default function Signup() {
 
         // Check if passwords match before sending response
         if (formData.password !== formData.confirmPassword) {
-            setPasswordsMatch(false);
             setFormSubmitted(true)
             return; // Prevent form submission if passwords don't match
         }
@@ -88,7 +87,7 @@ export default function Signup() {
                                     <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
                                     <input type="password" name="confirmPassword" id="confirm-password" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
                                     {/* Password matching validation */}
-                                    {setFormSubmitted && formData.password !== formData.confirmPassword && (
+                                    {formSubmitted && formData.password !== formData.confirmPassword && (
                                         <p className="text-red-500 text-sm mt-1">Passwords do not match</p>
                                     )}
                                 </div>
