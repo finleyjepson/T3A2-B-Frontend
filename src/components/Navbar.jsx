527b81f9d3181c6dc66be681caea2259818d7d38
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-23 13:54:36 +1100

    feat: profile photo when logged in with basic menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar({ isLoggedIn, handleLogout }) {
                 {/* Profile photo button */}
                     <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded={isMenuOpen} onClick={toggleMenu} data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                         <span className="sr-only">Open user menu</span>
-                        <img className="w-8 h-8 rounded-full" src="https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-pixel-art-corgi-cute-cartoon-png-image_10247338.png" alt="user photo" />
+                        <img className="w-8 h-8 rounded-full" src="https://emoji.beeimg.com/🍔/apple" alt="user photo" />
                     </button>
                 {/* Dropdown menu */}
                     <div className={`absolute top-full right-0 mt-2 z-50 ${isMenuOpen ? '' : 'hidden'} my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`} id="user-dropdown">
@@ -40,9 +40,6 @@ export default function Navbar({ isLoggedIn, handleLogout }) {
                             <span className="block text-sm text-gray-500 truncate dark:text-gray-400">email@address.com</span>
                         </div>
                     <ul className="py-2" aria-labelledby="user-menu-button">
-                        <li>
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-                        </li>
                         <li>
                             <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
                         </li>
