35428bda9df61b0210150edd3a0cf45bffc2b325
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-24 17:55:16 +1100

    feat: added token to event creation; fix event creation

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -35,30 +35,39 @@ export default function CreateEvent() {
         // Console log coordinates on submit from the coordinates state
         console.log(coords)
 
-        // try {
-        //     const response = await fetch('http://localhost:4000/events/', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify({
-        //             title: eventInfo.title,
-        //             description: eventInfo.description,
-        //             category: "65d7f3396459d46c88d58b9f",
-        //             date: eventInfo.date,
-        //             venue: eventInfo.venue,
-        //             coords: coords,
-        //             anime: eventInfo.date,
-        //             // createdBy: 
-        //             organiser: eventInfo.organiser,
-        //             price: eventInfo.price
-        //         }),
-        //     }) 
-        //     console.log(response)
-        // // Catch response:    
-        // } catch (error) {
-        //     console.error('Problem creating event', error.message);
-        // }
+        try {
+            // User token handling
+            const accessToken = sessionStorage.getItem('accessToken') // Retrieve the session's access token
+            console.log('Access token:', accessToken)
+            if (!accessToken) {
+                throw new Error('Access token not found. Please login.')
+            }
+
+            const response = await fetch('http://localhost:4000/events/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}` // Add the token in the request
+                },
+                body: JSON.stringify({
+                    title: eventInfo.title,
+                    description: eventInfo.description,
+                    category: "65d7f3396459d46c88d58b9f",
+                    date: eventInfo.date,
+                    venue: eventInfo.venue,
+                    // Commenting out coords as this is breaking event creation. Something to do with the mapping.
+                    // coords: coords,
+                    anime: eventInfo.date,
+                    // createdBy: 
+                    organiser: eventInfo.organiser,
+                    price: eventInfo.price
+                }),
+            }) 
+            console.log(response)
+        // Catch response:    
+        } catch (error) {
+            console.error('Problem creating event', error.message);
+        }
     }
 
     // Geocode getter function
@@ -69,12 +78,14 @@ export default function CreateEvent() {
             try {
                 // Make call to Google Maps Geocode, taking in 'venue' as parameter
                 const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${venue}&key=${api}`)
-            const data = await response.json()
-            // Extract lat and long and set to coords state
-            setCoords(data.results[0].geometry.location)
+                const data = await response.json()
+                console.log("Long / Lat:", data.results[0].geometry.location)
+                // Extract lat and long and set to coords state
+                setCoords(data.results[0].geometry.location)
             } catch(error) {
                 // This is the hacky bit; disguising error as 'listening for location'
                 console.log("Listening for location")
+                console.log("Error fetching geocode:", error)
             }
         }
 
