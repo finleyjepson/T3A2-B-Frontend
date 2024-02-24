d967533b904a66eaccf37bb393520af2a24831c7
NeggFriedRice <tomloo222@gmail.com>
2024-02-24 17:07:40 +1100

    feat: Styling change on hover to event list

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -6,9 +6,9 @@ export default function EventList({ events, max }) {
 	<>
 		<ul>
 			{events.slice(0, max).map((event, index) => (
-			<li key={index}>
+			<li key={index} className="odd:bg-white">
 				<Link to={`/events/${index}`}>
-					<div className="p-4 transition-all delay-150 hover:-translate-y-1 hover:scale-105 hover:rounded-lg  hover:bg-slate-200 hover:shadow-md">
+					<div className="p-4 transition-all delay-150 hover:-translate-y-1 hover:scale-105 hover:rounded-lg  hover:bg-purple-100 hover:shadow-lg">
 						<h1 className="text-xl">{event.title}</h1>
 						<p>{event.description}</p>
 						<p>{event.date}</p>
