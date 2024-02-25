0fa5f8a7a7a3f03afe6c2ccd9f3d48c352050d36
Finley <trinity.tringl@gmail.com>
2024-02-25 21:10:50 +1000

    refactor: moved profile dropdown to its own file

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <>
+            {/* Navigation bar */}
+            <nav className='bg-gray-50 dark:bg-gray-800'>
+                <div className='px-5 py-3'>
+                    <div className='flex items-center'>
+                        <ul className='flex flex-row font-medium mt-0 space-x-8 text-sm'>
+                            <li>
+                                {/* aria-current="page" is for screen readers to comprehend the current page as the Home page */}
+                                <Link to='/'>
+                                    <p className='text-gray-900 dark:text-white hover:underline'>Home</p>
+                                </Link>
+                                {/* <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a> */}
+                            </li>
+                            <li>
+                                <Link to='/events'>
+                                    <p className='text-gray-900 dark:text-white hover:underline'>Events</p>
+                                </Link>
+                            </li>
+                            <li>
+                                <a href='#' className='text-gray-900 dark:text-white hover:underline'>
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#' className='text-gray-900 dark:text-white hover:underline'>
+                                    Contact Us
+                                </a>
+                            </li>
+                            <li>
+                                <Link to='/users'>
+                                    <p className='text-gray-900 dark:text-white hover:underline'>Users</p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/events/new'>
+                                    <p className='text-gray-900 dark:text-white hover:underline'>Create Event</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
