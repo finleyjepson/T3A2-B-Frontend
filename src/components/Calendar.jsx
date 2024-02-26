b3dda4482a30ff06b2e0bfe3bd6b7b391523b885
Thomas Loo <tom@TomsPC>
2024-02-26 17:25:10 +1100

    feat: Add clickable link to events calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,9 +3,35 @@ import Fullcalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
+import { useNavigate } from "react-router-dom"
 
 export default function Calendar({ events }) {
-  console.log(events)
+
+  const navigate =  useNavigate()
+
+  function handleMouseEnter(arg) {
+    tippy(arg.el, {
+      content: arg.event.title,
+      allowHTML: true
+    })
+  }
+
+  function handleClick(event) {
+    const eventTitle = event.event.title
+    console.log(events)
+    let index 
+    events.some((event, idx) => {
+      if (event.title === eventTitle) {
+        index = idx
+        return true
+      }
+    })
+    console.log(index)
+    navigate(`/events/${index}`)
+  }
+ 
   return (
     <div className="w-1/3 h-1/3 p-4 border-solid border-gray-300 border-2 rounded-lg m-4">
       <Fullcalendar 
@@ -13,7 +39,10 @@ export default function Calendar({ events }) {
       initialView={"dayGridMonth"}
       height={"60vh"}
       displayEventTime={false}
-      events={events}/>
+      events={events}
+      eventMouseEnter={handleMouseEnter}
+      eventClick={handleClick}
+      />
     </div>
   )
 }
