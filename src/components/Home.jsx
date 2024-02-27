d051e55f16a74159ae54449bd7e6eca6c0c0ff20
Finley <trinity.tringl@gmail.com>
2024-02-28 02:32:15 +1000

    feat: added maps component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,14 @@ import UpcomingAnimeContainer from "./UpcomingAnimeContainer"
 import UpcomingEventsContainer from "./events/UpcomingEventsContainer"
 import PollContainer from "./PollContainer"
 import Calendar from "./Calendar"
+import Map from "./Maps"
 
 export default function Home({ events }) {
     return (
         <div className='flex flex-wrap justify-center'>
+            <div>
+                <Map events={ events } />
+            </div>
             <div>
                 <Calendar events={ events }/>
                 <UpcomingEventsContainer events={events} />
