d4e5a38cd8fe27e0c49b8c64a4564e7f6ed54361
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-27 15:48:00 +1100

    feat: contact us modal

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -4,11 +4,20 @@ import { useState, useEffect } from "react"
 export default function Navbar({ user, isLoggedin }) {
     const [isLoggedInState, setIsLoggedInState] = useState()
     const [userState, setUserState] = useState({})
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         setIsLoggedInState(isLoggedin)
         setUserState(user)
     }, [isLoggedin, user])
+
+    const handleContactClick = () => {
+        setShowModal(true);
+    }
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
     return (
         <>
@@ -35,9 +44,9 @@ export default function Navbar({ user, isLoggedin }) {
                                 </a>
                             </li>
                             <li>
-                                <a href='#' className='text-gray-900 dark:text-white hover:underline animate-in fade-in duration-2s'>
+                                <button onClick={handleContactClick} className='text-gray-900 dark:text-white hover:underline animate-in fade-in duration-2s'>
                                     Contact Us
-                                </a>
+                                </button>
                             </li>
                             {(isLoggedin && userState.isAdmin) && (
                                 <li>
@@ -64,6 +73,17 @@ export default function Navbar({ user, isLoggedin }) {
                     </div>
                 </div>
             </nav>
+
+            {/* Contact Modal */}
+                <div className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${showModal ? "" : "opacity-0 pointer-events-none"}`}>
+                    <div className="absolute inset-0 bg-black opacity-75" onClick={handleCloseModal}></div>
+                    <div className="relative bg-white rounded-lg p-8 mx-4 transform transition-all duration-300 opacity-100 scale-100">
+                        {/* <p className="text-xs"><button className="absolute top-4 right-4 text-gray-600" onClick={handleCloseModal}>Close</button></p> */}
+                        <h3 className="mb-4 text-xl font-semibold text-gray-900">We'd love to hear from you</h3>
+                        <p className="mb-4 text-gray-600">Have any feedback or questions? Need help with an event?<br />Feel free to email us at:</p>
+                        <p className="mb-4 text-primary-500 font-medium">email@example.com</p>
+                    </div>
+                </div>
         </>
     )
 }
