e09ca4a448e2d373d0ce4f5bd9e12c86bf11be83
NeggFriedRice <tomloo222@gmail.com>
2024-02-28 11:59:32 +1100

    chore: Fix edit event button placement

--- src/components/events/EventList.jsx
+++ src/components/events/EventList.jsx
@@ -21,22 +21,26 @@ export default function EventList({ events, max, user, isLoggedIn }) {
             <ul>
                 {events.slice(0, max).map((event, index) => (
                     <li key={index} className='odd:bg-white'>
-                        <Link to={`/events/${index}`}>
+                        
                             <div className='p-4 transition-all delay-150 hover:-translate-y-1 hover:scale-105 hover:rounded-lg  hover:bg-blue-100 hover:shadow-lg'>
-                                <div className="flex justify-between">
-                                    <p className="text-sm">{event.anime}</p>
-                                    <p className="text-sm">{dateMod(event.date)}</p>
+                                <Link to={`/events/${index}`}>
+                                    <div className="flex justify-between">
+                                        <p className="text-sm">{event.anime}</p>
+                                        <p className="text-sm">{dateMod(event.date)}</p>
+                                    </div>
+                                    <h1 className='text-xl py-2'>{event.title}</h1>
+                                    <p>{event.description}</p>
+                                </Link>
+                                <div className="flex justify-end">
+                                    {isLoggedInState && (userState.isAdmin || (userState.isOrganiser && user._id === events.organiserId)) && (
+                                    <button className='bg-red-600 py-1 px-2 hover:bg-red-500 rounded-md text-white hover:rounded-md' onClick={() => navigate(`/events/edit/${event._id}`)}>
+                                        Edit Event
+                                    </button>
+                                    )}
                                 </div>
-                                <h1 className='text-xl py-2'>{event.title}</h1>
-                                <p>{event.description}</p>
-                                
                             </div>
-                        </Link>
-                        {isLoggedInState && (userState.isAdmin || (userState.isOrganiser && user._id === events.organiserId)) && (
-                            <button className='bg-red-600 py-1 px-2 rounded-md text-white' onClick={() => navigate(`/events/edit/${event._id}`)}>
-                                Edit Event
-                            </button>
-                        )}
+
+
                     </li>
                 ))}
             </ul>
